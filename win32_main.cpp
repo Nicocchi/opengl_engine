08ee4b6f99d848dd8a520030dff1341daed5ba63
Nicocchi <nicocchilover@gmail.com>
2024-10-26 11:30:15 -0400

    changed window gl initialization to work on both windows 10 and 11

--- win32_main.cpp
+++ win32_main.cpp
@@ -25,6 +25,8 @@ if(!(exp)) {*(int*)0 = 0;}
 global_variable PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatARB = NULL;
 global_variable PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsARB = NULL;
 global_variable PFNWGLSWAPINTERVALEXTPROC wglSwapIntervalEXT = NULL;
+global_variable HWND window;
+global_variable HDC dc;
 
 internal void 
 FatalError(const char* message)
@@ -158,101 +160,28 @@ WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
     return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 
+void *platform_load_gl_function(char *funName)
+{
+    PROC proc = wglGetProcAddress(funName);
+    if (!proc)
+    {
+        static HMODULE openglDLL = LoadLibraryA("opengl32.dll");
+        proc = GetProcAddress(openglDLL, funName);
+
+        if (!proc)
+        {
+            FatalError("Failed to load gl function");
+            return nullptr;
+        }
+    }
+
+    return (void*)proc;
+}
+
 internal void
 Win32GetWglFunctions()
 {
-	// to get WGL functions we need valid GL context, so create dummy window for dummy GL context
-    HWND dummy = CreateWindowExW(
-								 0, L"STATIC", L"DummyWindow", WS_OVERLAPPED,
-								 CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-								 NULL, NULL, NULL, NULL);
-    Assert(dummy && "Failed to create dummy window");
-	
-    HDC dc = GetDC(dummy);
-    Assert(dc && "Failed to get device context for dummy window");
-	
-    PIXELFORMATDESCRIPTOR desc = {};
-	desc.nSize = sizeof(desc);
-	desc.nVersion = 1;
-	desc.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
-	desc.iPixelType = PFD_TYPE_RGBA;
-	desc.cColorBits = 24;
-	
-    int format = ChoosePixelFormat(dc, &desc);
-    if (!format)
-    {
-        FatalError("Cannot choose OpenGL pixel format for dummy window!");
-    }
-	
-    int ok = DescribePixelFormat(dc, format, sizeof(desc), &desc);
-    Assert(ok && "Failed to describe OpenGL pixel format");
-	
-    // reason to create dummy window is that SetPixelFormat can be called only once for the window
-    if (!SetPixelFormat(dc, format, &desc))
-    {
-        FatalError("Cannot set OpenGL pixel format for dummy window!");
-    }
-	
-    HGLRC rc = wglCreateContext(dc);
-    Assert(rc && "Failed to create OpenGL context for dummy window");
-	
-    ok = wglMakeCurrent(dc, rc);
-    Assert(ok && "Failed to make current OpenGL context for dummy window");
-	
-    // https://www.khronos.org/registry/OpenGL/extensions/ARB/WGL_ARB_extensions_string.txt
-    PFNWGLGETEXTENSIONSSTRINGARBPROC wglGetExtensionsStringARB =
-	(PFNWGLGETEXTENSIONSSTRINGARBPROC)wglGetProcAddress("wglGetExtensionsStringARB");
-    if (!wglGetExtensionsStringARB)
-    {
-        FatalError("OpenGL does not support WGL_ARB_extensions_string extension!");
-    }
-	
-    const char* ext = wglGetExtensionsStringARB(dc);
-    Assert(ext && "Failed to get OpenGL WGL extension string");
-	
-    const char* start = ext;
-    for (;;)
-    {
-        while (*ext != 0 && *ext != ' ')
-        {
-            ext++;
-        }
-		
-        size_t length = ext - start;
-        if (StringsAreEqual("WGL_ARB_pixel_format", start, length))
-        {
-            // https://www.khronos.org/registry/OpenGL/extensions/ARB/WGL_ARB_pixel_format.txt
-            wglChoosePixelFormatARB = (PFNWGLCHOOSEPIXELFORMATARBPROC)wglGetProcAddress("wglChoosePixelFormatARB");
-        }
-        else if (StringsAreEqual("WGL_ARB_create_context", start, length))
-        {
-            // https://www.khronos.org/registry/OpenGL/extensions/ARB/WGL_ARB_create_context.txt
-            wglCreateContextAttribsARB = (PFNWGLCREATECONTEXTATTRIBSARBPROC)wglGetProcAddress("wglCreateContextAttribsARB");
-        }
-        else if (StringsAreEqual("WGL_EXT_swap_control", start, length))
-        {
-            // https://www.khronos.org/registry/OpenGL/extensions/EXT/WGL_EXT_swap_control.txt
-            wglSwapIntervalEXT = (PFNWGLSWAPINTERVALEXTPROC)wglGetProcAddress("wglSwapIntervalEXT");
-        }
-		
-        if (*ext == 0)
-        {
-            break;
-        }
-		
-        ext++;
-        start = ext;
-    }
-	
-    if (!wglChoosePixelFormatARB || !wglCreateContextAttribsARB || !wglSwapIntervalEXT)
-    {
-        FatalError("OpenGL does not support required WGL extensions for modern context!");
-    }
-	
-    wglMakeCurrent(NULL, NULL);
-    wglDeleteContext(rc);
-    ReleaseDC(dummy, dc);
-    DestroyWindow(dummy);
+    
 }
 
 int CALLBACK
@@ -260,10 +189,8 @@ WinMain(HINSTANCE Instance,
         HINSTANCE PrevInstance,
         LPSTR CommandLine,
         int ShowCode)
-{
-	Win32GetWglFunctions();
-	
-	WNDCLASSEX wc = 
+{    
+    WNDCLASSEX wc = 
     { 
         sizeof(WNDCLASSEX), 
         CS_HREDRAW | CS_VREDRAW | CS_OWNDC | CS_DBLCLKS, 
@@ -274,26 +201,96 @@ WinMain(HINSTANCE Instance,
         "Engine", 
         NULL 
     };
-	
-	RegisterClassEx(&wc);
-	
-    HWND hwnd = CreateWindowEx(WS_EX_NOREDIRECTIONBITMAP,
-                               wc.lpszClassName, "Engine",
-                               WS_CAPTION | WS_SYSMENU | WS_CLIPSIBLINGS | WS_CLIPCHILDREN,
-                               CW_USEDEFAULT, CW_USEDEFAULT,
-                               CW_USEDEFAULT, CW_USEDEFAULT,
-                               NULL, NULL, wc.hInstance, NULL);
+
+    PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatARB = nullptr;
+    PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsARB = nullptr;
+
+    int dwStyle = WS_OVERLAPPEDWINDOW;
+
+    // Fake window initializing OpenGL
+    {
+        window = CreateWindowExW(
+								 0, L"STATIC", L"DummyWindow", WS_OVERLAPPED,
+								 CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+								 NULL, NULL, NULL, NULL);
+        
+        if (window == NULL)
+        {
+            FatalError("Failed to create dummy window");
+        }
+
+        HDC fakeDC = GetDC(window);
+        if (!fakeDC)
+        {
+            FatalError("Failed to get HDC");
+        }
+
+        PIXELFORMATDESCRIPTOR pfd = {0};
+        pfd.nSize = sizeof(pfd);
+        pfd.nVersion = 1;
+        pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+        pfd.iPixelType = PFD_TYPE_RGBA;
+        pfd.cColorBits = 32;
+        pfd.cAlphaBits = 8;
+        pfd.cDepthBits = 24;
+
+        int pixelFormat = ChoosePixelFormat(fakeDC, &pfd);
+        if (!pixelFormat)
+        {
+            FatalError("Failed to choose pixel format");
+        }
+
+        if (!SetPixelFormat(fakeDC, pixelFormat, &pfd))
+        {
+            FatalError("Failed to set pixel format");
+        }
+
+        // Create a Handle to a fake OpenGL Rendering Context
+        HGLRC fakeRC = wglCreateContext(fakeDC);
+        if (!fakeRC)
+        {
+            FatalError("Failed to create render context");
+        }
+
+        if (!wglMakeCurrent(fakeDC, fakeRC))
+        {
+            FatalError("Failed to make current");
+        }
+
+        wglChoosePixelFormatARB = 
+            (PFNWGLCHOOSEPIXELFORMATARBPROC)platform_load_gl_function("wglChoosePixelFormatARB");
+        wglCreateContextAttribsARB =
+            (PFNWGLCREATECONTEXTATTRIBSARBPROC)platform_load_gl_function("wglCreateContextAttribsARB");
+        wglSwapIntervalEXT =
+            (PFNWGLSWAPINTERVALEXTPROC)platform_load_gl_function("wglSwapIntervalEXT");
+        
+        if (!wglCreateContextAttribsARB || !wglChoosePixelFormatARB)
+        {
+            FatalError("Failed to load OpenGL functions");
+        }
+
+        // Clean up the fake stuff
+        wglMakeCurrent(fakeDC, 0);
+        wglDeleteContext(fakeRC);
+        ReleaseDC(window, fakeDC);
+
+        // Can't reuse the same (Device)Context because
+        // we already called "SetPixelFormat"
+        DestroyWindow(window);
+    }
+
+    if (!RegisterClassEx(&wc))
+    {
+        FatalError("RegisterClassEX failed");
+    }
     
     i32 width = CW_USEDEFAULT;
     i32 height = CW_USEDEFAULT;
     
     platform_engine engine = {};
     platform_render render = {};
-    
+
     engine.running = true;
-    
-    ShowWindow(hwnd, SW_SHOWDEFAULT);
-    UpdateWindow(hwnd);
     
     {
         platform_memory* memory = &engine.memory;
@@ -322,74 +319,85 @@ WinMain(HINSTANCE Instance,
         engine.vsSource = _shader_resource_vs46;
         engine.psSource = _shader_resource_ps46;
     }
-	
-    HDC dc = GetDC(hwnd);
-	{
-		i32 pixelAttribs[] = 
-		{
-			WGL_DRAW_TO_WINDOW_ARB, GL_TRUE,
+    
+    // Real window initializing OpenGL
+    {
+        window = CreateWindowEx(0, wc.lpszClassName, "Engine", dwStyle,
+                                100, 100, width, height, NULL, NULL, wc.hInstance, NULL);
+        
+        if (window == NULL)
+        {
+            FatalError("Failed to create real window");
+        }
+
+        dc = GetDC(window);
+        if (!dc)
+        {
+            FatalError("Failed to get DC");
+        }
+
+        const int pixelAttribs[] =
+        {
+            WGL_DRAW_TO_WINDOW_ARB, GL_TRUE,
             WGL_SUPPORT_OPENGL_ARB, GL_TRUE,
             WGL_DOUBLE_BUFFER_ARB,  GL_TRUE,
+            WGL_SWAP_METHOD_ARB,    WGL_SWAP_COPY_ARB,
             WGL_PIXEL_TYPE_ARB,     WGL_TYPE_RGBA_ARB,
-            WGL_COLOR_BITS_ARB,     24,
+            WGL_ACCELERATION_ARB,   WGL_FULL_ACCELERATION_ARB,
+            WGL_COLOR_BITS_ARB,     32,
+            WGL_ALPHA_BITS_ARB,     8,
             WGL_DEPTH_BITS_ARB,     24,
-            WGL_STENCIL_BITS_ARB,   8,
-			0,
-		};
-		
-		i32 pixelFormatID;
-		u32 numFormats;
-		b32 status = wglChoosePixelFormatARB(dc, pixelAttribs, NULL, 1, &pixelFormatID, &numFormats);
-		
-		if(status == false || numFormats == 0)
-		{
-			return 1;
-		}
-		
-		PIXELFORMATDESCRIPTOR pfd;
-		DescribePixelFormat(dc, pixelFormatID, sizeof(pfd), &pfd);
-		SetPixelFormat(dc, pixelFormatID, &pfd);
-	}
-	
-	{
-		i32 major_min = 4, minor_min = 5;
-		i32 contextAttribs[] = 
-		{
-			WGL_CONTEXT_MAJOR_VERSION_ARB, 4,
-            WGL_CONTEXT_MINOR_VERSION_ARB, 5,
-            WGL_CONTEXT_PROFILE_MASK_ARB,  WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
-#ifdef DEBUG
-            // ask for debug context for non "Release" builds
-            // this is so we can enable debug callback
+            0
+        };
+
+        UINT numPixelFormats;
+        int pixelFormat = 0;
+        if (!wglChoosePixelFormatARB(dc, pixelAttribs,
+                                    0, // Float list
+                                    1, // Max formats
+                                    &pixelFormat,
+                                    &numPixelFormats))
+        {
+            FatalError("Failed to wglChoosePixelFormatARB");
+            return false;
+        }
+
+        PIXELFORMATDESCRIPTOR pfd = {0};
+        DescribePixelFormat(dc, pixelFormat, sizeof(PIXELFORMATDESCRIPTOR), &pfd);
+
+        if (!SetPixelFormat(dc, pixelFormat, &pfd))
+        {
+            FatalError("Failed to SetPixelFormat");
+        }
+
+        const int contextAttribs[] =
+        {
+            WGL_CONTEXT_MAJOR_VERSION_ARB, 4,
+            WGL_CONTEXT_MINOR_VERSION_ARB, 3,
+            WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
             WGL_CONTEXT_FLAGS_ARB, WGL_CONTEXT_DEBUG_BIT_ARB,
-#endif
-            0,
-		};
-		
-		HGLRC rc = wglCreateContextAttribsARB(dc, 0, contextAttribs);
-		if(!rc)
-		{
-			return 1;
-		}
-		
-		if(!wglMakeCurrent(dc, rc))
-		{
-			return 1;
-		}
-		
-		if(!gladLoadGL())
+            0
+        };
+
+        HGLRC rc = wglCreateContextAttribsARB(dc, 0, contextAttribs);
+        if (!rc)
+        {
+            FatalError("Failed to create Render context for OpenGL");
+        }
+
+        if (!wglMakeCurrent(dc, rc))
+        {
+            FatalError("Failed to wglMakeCurrent");
+        }
+
+        if(!gladLoadGL())
 		{
 			return -1;
 		}
-		
-#ifdef DEBUG
-		// enable debug callback
-		glDebugMessageCallback(&DebugCallback, NULL);
-		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
-#endif
-		
-	}
-    
+    }
+
+    ShowWindow(window, SW_SHOW);
+
     PFNWGLGETEXTENSIONSSTRINGEXTPROC _wglGetExtensionsStringEXT = 
 	(PFNWGLGETEXTENSIONSSTRINGEXTPROC)wglGetProcAddress("wglGetExtensionsStringEXT");
     b32 swapControlSupported = strstr(_wglGetExtensionsStringEXT(), "WGL_EXT_swap_control") != 0;
@@ -413,6 +421,7 @@ WinMain(HINSTANCE Instance,
 		OutputDebugStringA("WGL_EXT_swap_control not supported \n");
 	}
     
+    // -------------------------------------------------------------------------
     render_Init(&engine, &render);
     
     LARGE_INTEGER clock;
@@ -421,7 +430,7 @@ WinMain(HINSTANCE Instance,
     QueryPerformanceCounter(&clock);
     
     WINDOWPLACEMENT plac;
-    GetWindowPlacement(hwnd, &plac);
+    GetWindowPlacement(window, &plac);
     engine.posX = plac.rcNormalPosition.left;
     engine.posY = plac.rcNormalPosition.top;
     
@@ -434,15 +443,15 @@ WinMain(HINSTANCE Instance,
             {
                 case WM_LBUTTONDOWN:
                 {
-                }break;
+                } break;
                 case WM_LBUTTONUP:
                 {
-                }break;
-                
+                } break;
+
                 case WM_QUIT:
                 {
                     engine.running = false;
-                }break;
+                } break;
             }
             
             TranslateMessage(&msg);
@@ -450,12 +459,14 @@ WinMain(HINSTANCE Instance,
         }
         
         RECT rect;
-        GetClientRect(hwnd, &rect);
+        GetClientRect(window, &rect);
         width = rect.right - rect.left;
 		height = rect.bottom - rect.top;
         
         f64 delta = Win32UpdateClock(&clock, freq.QuadPart);
         engine.time += delta;
+
+        engine.gameUpdate(&engine, delta);
         
         if(width != engine.windowWidth || height != engine.windowHeight)
         {
@@ -469,8 +480,8 @@ WinMain(HINSTANCE Instance,
         //if (vsynch != 0) glFinish();
     }
     
-    DestroyWindow(hwnd);
-    UnregisterClass(wc.lpszClassName, wc.hInstance);
+    DestroyWindow(window);
+    // UnregisterClass(wc.lpszClassName, wc.hInstance);
     
     return 0;
 }
